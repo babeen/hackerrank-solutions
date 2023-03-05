eef7feb86924516667abce0b9f0bacac4faf3c8b
babinnepali <babinnepalibtl@gmail.com>
2023-03-05 23:20:56 +0545

    namaste js

---
+++ namaste.js
@@ -0,0 +1,21 @@
+// fetch("https://a").then(function cbF(){
+//     console.log("CB Nepse");
+// })
+
+// let bike = {
+//     brake:'disbrake',
+//     tyre:'mrf',
+//     brand:'honda',
+//     light:'bright',
+//     start: function(){
+//         console.log('Insert the key and hold cluch and press power key. \n The engine start....');
+//     }()
+// }
+
+// console.log(bike.start);
+
+const nameMy = 'babin singh nepali';
+
+for(let i of nameMy){
+    console.log(i);
+}
