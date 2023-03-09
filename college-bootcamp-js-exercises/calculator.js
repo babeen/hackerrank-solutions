34f0068e2e857e5c782275176b1202783e0fd234
babeen <babinnepalibtl@gmail.com>
2023-03-09 22:21:09 +0545

    reducer example

--- college-bootcamp-js-exercises/calculator.js
+++ college-bootcamp-js-exercises/calculator.js
@@ -1,1 +1,8 @@
-//how to connect to mongodb in nodejs?
+//map()
+const numbers = [1,2,3];
+console.log('Normal array \n',numbers);
+const doubleNumbers = numbers.map(num=>num*2);
+console.log('Mapped array \n', doubleNumbers)
+
+//reduce()
+
